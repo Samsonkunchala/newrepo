391107d91c2582461b67b7f9995097cdff5c86c6
SamsonKunchala <samsonkunchala297@gmail.com>
2026-04-16 15:17:19 +0530

    changes

--- src/main/java/com/myproject/HelloJenkins.java
+++ src/main/java/com/myproject/HelloJenkins.java
@@ -1,3 +1,5 @@
+package com.myproject;
+
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello! This is my Jenkins CI/CD pipeline server!!");
--- src/main/java/com/myproject/Project.java
+++
@@ -1,5 +0,0 @@
-public class HelloJenkins {
-    public static void main(String[] args) {
-        System.out.println("Hello! This is my Jenkins CI/CD pipeline server!!");
-    }
-}
